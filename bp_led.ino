22d6171381fadab42f71bc585276cd44ba77339a
Samir Bouaked <samir.bouaked@gmail.com>
2015-06-22 14:44:08 +0200

    script arduino led

--- bp_led.ino
+++ bp_led.ino
@@ -1,22 +1,25 @@
 
 // constants won't change. They're used here to
 // set pin numbers:
-const int buttonPin = 3;     // the number of the pushbutton pin
-const int ledPin =  2;      // the number of the LED pin
+const int buttonPin = 3;        // the number of the pushbutton pin
+const int ledPin =  2;          // the number of the LED pin
+const String CONST_ID = "01";   // the main Id 
+const String CONST_TYPE = "01"; // the id of press button
 
 
 
 // variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+int buttonState = 0;            // variable for reading the pushbutton status
 int ledState = 0;
 int currenteState = 0;
 
+
 void setup() {
+  Serial.begin(9600);
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);     
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
-  Serial.print("By SB & LB");     
 }
 
 
@@ -43,11 +46,11 @@ void loop(){
 // turn LED on:
 void turnOn(){
    digitalWrite(ledPin, HIGH);
-   Serial.println("LED ON");
+   Serial.println(CONST_ID+":"+CONST_TYPE+":1");
 }
 
 // turn LED Off:
 void turnOff(){
   digitalWrite(ledPin, LOW);
-  Serial.println("LED OFF");
+  Serial.println(CONST_ID+":"+CONST_TYPE+":2");
 }
