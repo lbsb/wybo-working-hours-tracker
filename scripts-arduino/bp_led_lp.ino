c06474f2c553b4c21e3b67c1d7b904395f952357
Samir Bouaked <samir.bouaked@gmail.com>
2015-06-23 17:19:14 +0200

    add comments

--- scripts-arduino/bp_led_lp.ino
+++ scripts-arduino/bp_led_lp.ino
@@ -1,13 +1,8 @@
-#define UBRRH
-#include "Platform.h" // Arduino symbols declarations
-
-// constants won't change. They're used here to
-// set pin numbers:
+// #### Constantes ###
 const int buttonPin = 3;        // the number of the pushbutton pin
 const int ledPin =  2;          // the number of the LED pin
 const String CONST_ID = "01";   // the main Id
 const String CONST_TYPE = "01"; // the id of press button
-
 
 
 // variables will change:
@@ -15,10 +10,14 @@ int buttonState = 0;            // variable for reading the pushbutton status
 int ledState = 0;
 int currenteState = 0;
 
-enum { EV_NONE=0, EV_SHORTPRESS, EV_LONGPRESS };
+enum {
+  EV_NONE=0,
+  EV_SHORTPRESS,
+  EV_LONGPRESS
+};
 
-boolean button_was_pressed; // previous state
-int button_pressed_counter; // press running duration
+boolean button_was_pressed;     // previous state
+int button_pressed_counter;     // press running duration
 
 // ############## SETUP ############################
 
@@ -26,8 +25,8 @@ void setup() {
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);        // initialize the LED pin as an output:
   pinMode(buttonPin, INPUT);      // initialize the pushbutton pin as an input:
-  button_was_pressed = false;
-  button_pressed_counter = 0;
+  button_was_pressed = false;     // initialize the pushbutton to false (not pressed)
+  button_pressed_counter = 0;     // initialize the pusbutton counter
 }
 
 // ############## LOOP ############################
