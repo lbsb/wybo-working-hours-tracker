b910a3acccd65088fb89a9bdc00e907b05f27806
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-06-24 12:24:32 +0200

    Add synchronisation feature

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 import serial
 import socket
@@ -6,6 +7,7 @@ import time
 import logging
 import json
 import sys
+import thread
 import cPickle as pickle
 from user import User
 
@@ -55,15 +57,12 @@ class Client(object):
         self._logger.addHandler(file_handler)
         self._logger.addHandler(console_handler)
         self._logger.setLevel(logging.DEBUG)
+        # init socket connection
+        self._init_socket()
         # init user
         self._read_user_settings()
         # init serial connection
         self._init_serial_port()
-        # init socket connection
-        try:
-            self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        except socket.error as msg:
-            self._logger.debug("Can't init socket : %s", msg)
 
     def _read_user_settings(self):
         """
@@ -100,7 +99,11 @@ class Client(object):
         # sync user settings and get back generated id
         user._id = self._sync_user_settings(user)
         # save user settings
-        self._save_user_settings(user)
+        if user._id != 0:
+            self._save_user_settings(user)
+            self._logger.info("Successful synchronization")
+        else:
+            self._logger.info("Synchronisation failed. Please check your internet and serial connection. and retry")
 
     def _sync_user_settings(self, user):
         """
@@ -109,14 +112,31 @@ class Client(object):
 
         # serialize user object
         data_user = pickle.dumps(user, -1)
+        # send init protocol
+        self._socket.sendto("00:4", (SERVER_IP, SERVER_PORT))
+        self._logger.debug("message : \"00:4\" has been sent to %s:%d", SERVER_IP, SERVER_PORT)
 
-        # send user to server
-        self._socket.sendto(data_user, (SERVER_IP, SERVER_PORT))
-        # receive id
+        # wait responsove from server
         data = self._socket.recv(1024)
-        print "%s" % data
+        self._logger.debug("data : %s has been received from server", data)
 
-        return data.replace("\n", "")
+        if re.compile("4").match(data.replace("\n", "")):
+            # send user to server
+            self._socket.sendto(data_user, (SERVER_IP, SERVER_PORT))
+            self._logger.debug("data : %s has been send from server", data_user)
+            # receive id
+            data_user_id = self._socket.recv(1024)
+            self._logger.debug("Id : %s has been send from server", data_user_id)
+
+            # format user id
+            data_user_id = data_user_id.replace("\n", "")
+            # send confirmation to the server by sending the same user id
+            self._socket.sendto(data_user_id, (SERVER_IP, SERVER_PORT))
+            self._logger.debug("Confirmation id : \"%s\" sent to server", data_user_id)
+
+            return int(data_user_id)
+
+        return 0
 
     def _save_user_settings(self, user):
         """
@@ -130,15 +150,28 @@ class Client(object):
                 'first_name': user._first_name.replace("\n", ""),
                 'last_name': user._last_name.replace("\n", ""),
                 'email': user._email.replace("\n", "")
-            }, outfile, indent = 4)
+            }, outfile, indent = 4, encoding = 'utf-8')
+
+        self._logger.debug("User settings has been saved in %s", (USER_FILE_LOCATION + USER_FILE_NAME))
 
         # set user information in current user
         self._user = user
+
+    def _init_socket(self):
+        """
+        Init socket
+        """
+
+        try:
+            self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        except socket.error as msg:
+            self._logger.debug("Can't init socket : %s", msg)
 
     def _init_serial_port(self):
         """
         Init serial port
         """
+
         self._logger.info("Arduino disconnected. Please connect it on USB port : %s", CLIENT_SERIAL_PORT)
         while self._serial_port == None:
             try:
@@ -163,7 +196,7 @@ class Client(object):
                 # try to reconnect
                 self._serial_port.open()
                 self._logger.info("Arduino reconnected")
-                self._send_sensor_status_to_server("01:0")
+                self._send_sensor_status_to_server("0")
             except OSError as msg:
                 self._logger.debug("Reconnection failed : %s", msg)
             except serial.SerialException as msg:
@@ -199,7 +232,7 @@ class Client(object):
                 self._serial_port.close()
                 self._logger.debug("Serial port : \"%s\" has been closed", CLIENT_SERIAL_PORT)
                 # send last message if the sensor is disconnected
-                self._send_sensor_status_to_server("01:2")
+                self._send_sensor_status_to_server("2")
                 self._logger.debug("Disconnected status has been sent to the server")
                 self._reconnect_serial_port()
 
@@ -210,21 +243,20 @@ class Client(object):
         Z : sensor value
         :param message:
         """
-    
+
         self._socket.sendto(sensor_value, (SERVER_IP, SERVER_PORT))
         self._logger.debug("message : \"%s\" has been sent to %s:%d", sensor_value, SERVER_IP, SERVER_PORT)
 
+    def _check_server_connection(self):
+        """
+        Ping server to check if it's available
+        :return:
+        """
 
-def _check_server_connection(self):
-    """
-    Ping server to check if it's available
-    :return:
-    """
+        if os.system("ping -c 1 " + SERVER_IP) == 0:
+            self._logger.debug("server is online")
+        else:
+            self._logger.debug("server is offline")
 
-    if os.system("ping -c 1 " + SERVER_IP) == 0:
-        self._logger.debug("server is online")
-    else:
-        self._logger.debug("server is offline")
-
-    # short delay between two tentatives
-    time.sleep(SERVER_PING_DELAY)
+        # short delay between two tentatives
+        time.sleep(SERVER_PING_DELAY)
