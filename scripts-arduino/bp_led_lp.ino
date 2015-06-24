2756a6375f352f9868d11f289c4ca3318961558e
Samir Bouaked <samir.bouaked@gmail.com>
2015-06-24 08:46:37 +0200

    ajout comments temp pour Louis

--- scripts-arduino/bp_led_lp.ino
+++ scripts-arduino/bp_led_lp.ino
@@ -38,7 +38,6 @@ void loop(){
     switch(eventResult){
         case EV_NONE:               // if nothing append
             //TODO : Ne rien faire de particulier, ou attendre la fin d'une synchronisation
-            
             break;
         case EV_SHORTPRESS:         // if short press
             //TODO : Allumer ou éteindre la LED (a check)
@@ -51,37 +50,30 @@ void loop(){
             break;
         case EV_LONGPRESS:          // if long press
             //TODO : Faire clignoter la LED et attendre la fin d'une synchronisation.
-            
             break;
     }
-
     delay(DELAY);
 }
 
 // ####### Function ########
 int handle_button(){
     int event;
-    int button_now_pressed = !digitalRead(BUTTON_PIN); // pin low -> pressed
+    int button_now_pressed = !digitalRead(BUTTON_PIN);  // pin low -> pressed
 
-    if (!button_now_pressed && button_was_pressed) {
-        if (button_pressed_counter < LONGPRESS_LEN){
-            event = EV_SHORTPRESS;
-        }
+    if (!button_now_pressed && button_was_pressed) {    // TODO : TEMP COMMENT : Si button n'est pas pressé mais l'était avant
+        if (button_pressed_counter < LONGPRESS_LEN)     // et si le counter n'avait pas dépassé la limite counter
+            event = EV_SHORTPRESS;                      // alors c'est un short press
         else{
-            event = EV_LONGPRESS;
+            event = EV_LONGPRESS;                       // Si il a dépassé la limite alors long press
             Serial.println(CONST_ID+":"+CONST_TYPE+":4");
-            
         }
     }
-    else{
+    else                                                // Sinon si le bouton est pressé ou n'était pas pressé avant
             event = EV_NONE;
-    }
-
-    if (button_now_pressed)
+    if (button_now_pressed)                             // Si le button est pressé alors on incremente le counter
         ++button_pressed_counter;
-    else 
-        button_pressed_counter = 0; 
-        
+    else                                                // Sinon si le button n'est pas pressé
+        button_pressed_counter = 0;                     // On met le counter a zéro
     button_was_pressed = button_now_pressed;
     return event;
 }
@@ -92,7 +84,6 @@ void turnOn(){
     digitalWrite(LED_PIN, HIGH);
     Serial.println(CONST_ID+":"+CONST_TYPE+":1");
 }
-
 // turn LED Off:
 void turnOff(){
     digitalWrite(LED_PIN, LOW);
