71c771fb5861872666e0798ef2344935e86a63a0
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-06-24 15:24:01 +0200

    add uuid attribute in User class

--- user.py
+++ user.py
@@ -3,9 +3,11 @@ class User(object):
     _first_name = None
     _last_name = None
     _email = None
+    _uuid = None
 
-    def __init__(self, id = "", first_name = "", last_name = "", email = ""):
+    def __init__(self, id = "", first_name = "", last_name = "", email = "", uuid = ""):
         self._id = id
         self._first_name = first_name
         self._last_name = last_name
         self._email = email
+        self._uuid = uuid
