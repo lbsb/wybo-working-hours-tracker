26ea7e77ec331a99e7b8c4476c8ae785397a027c
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-06-25 20:13:20 +0200

    add password attribute to User class

--- user.py
+++ user.py
@@ -6,10 +6,13 @@ class User(object):
     _last_name = None
     _email = None
     _uuid = None
+    _password = None
 
-    def __init__(self, id = "", first_name = "", last_name = "", email = "", uuid = ""):
+    def __init__(self, id = "", first_name = "", last_name = "", email = "", uuid = "", password =""):
         self._id = id
         self._first_name = first_name
         self._last_name = last_name
         self._email = email
         self._uuid = uuid
+        self._password = password
+
