e64714eca7c9144d4fc4347070fa5ffdd7e21b75
Samir Bouaked <samir.bouaked@gmail.com>
2015-06-26 10:36:52 +0200

    add logs

--- app/liste/liste.js
+++ app/liste/liste.js
@@ -11,10 +11,18 @@ angular.module('myApp.liste', ['ngRoute'])
 
 .controller('View1Ctrl', ['$scope','$http', function($scope,$http) {
         //To get the data of the json file
-        $http.get('data/logs.json').success(function(data,status){
-            $scope.items=data;
+        $http.get('data/users.json').success(function(data,status){
+            $scope.users=data;
             $scope.status=status;
         }).error(function(data, status) { $scope.data = data || "Request failed"; $scope.status = status;
+        });
+    }])
+    .controller('DetailCtrl', ['$scope','$http', function($scope,$http) {
+        //To get the data of the json file
+        $http.get('data/detail.json').success(function(detailData,status){
+            $scope.details=detailData;
+            $scope.status=status;
+        }).error(function(detailData, status) { $scope.detailData = detailData || "Request failed"; $scope.status = status;
         });
     }]);
 
